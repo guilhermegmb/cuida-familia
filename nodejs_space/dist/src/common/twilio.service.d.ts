b8c12f0f867901297547cb21d6b478a9a64c4e14
guilhermegmb <guilhermegmb@gmail.com>
2026-05-05 12:56:07 -0300

    Delete nodejs_space directory

--- nodejs_space/dist/src/common/twilio.service.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class TwilioService {
-    validateSignature(signature: string | undefined, url: string, params: Record<string, any>): boolean;
-    twiml(message: string): string;
-}
